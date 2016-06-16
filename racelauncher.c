7f6b5754db5e461b84c998b3c01e52a03750179e
Clay Dowling <clay@lazarusid.com>
2016-06-16 13:19:55 -0400

    First POC

---
+++ racelauncher.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int launch_program(const char *workingdir, const char *executable,
+		char * const*argv)
+{
+	pid_t pid;
+	int status;
+	int died;
+
+	switch(pid = fork()) {
+	case -1:
+		perror("fork");
+		exit(EXIT_FAILURE);
+	case 0:
+		if (chdir(workingdir)) {
+			perror(workingdir);
+			exit(EXIT_FAILURE);
+		}
+		execvp(executable, argv);
+	default:
+		died = wait(&status);
+		return status;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	char *coderace;
+	char *kata;
+	char *home;
+	char startrace[256];
+	char openfile[256];
+	char * const* startrace_params[3];
+	char * const* gedit_params[3];
+	int status;
+
+	home = getenv("HOME");
+	coderace = getenv("CODERACE");
+	kata = getenv("KATA");
+	
+	snprintf(startrace, sizeof(startrace), "%s/setupRace.sh", coderace);
+	snprintf(openfile, sizeof(openfile), "%s/%s/src/RomanNumerals.js",
+			coderace, kata);
+
+	startrace_params[0] = startrace;
+	startrace_params[1] = kata;
+	startrace_params[2] = NULL;
+
+	gedit_params[0] = "gedit";
+	gedit_params[1] = "openfile";
+	gedit_params[2] = NULL;
+
+	if ((status = launch_program(coderace, startrace, startrace_params))) {
+		return status;
+	}
+	return launch_program(home, "gedit", gedit_params);
+
+}
